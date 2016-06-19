99995950b0750452173e34b91f98f57605fb51a2
Erik Vanderlei Ferandes <erik.vanderlei.programador@outlook.com>
2016-06-19 17:51:24 -0300

    Create the sass task on 'gulpfile.babel.js'

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,5 +1,31 @@
 import gulp from 'gulp';
+import sass from 'gulp-sass';
+import autoprefixer from 'gulp-autoprefixer';
+import cssNano from 'gulp-cssnano';
 
-gulp.task('default', () => {
-  console.log("ok");
+const app = './app/';
+
+const sassFiles = [
+  app + 'sass/main.sass',
+  app + 'sass/media.sass',
+];
+
+gulp.task('sass', () => {
+  const AUTOPREFIXER_BROWSERS = [
+    'ie >= 10',
+    'ie_mob >= 10',
+    'ff >= 30',
+    'chrome >= 34',
+    'safari >= 7',
+    'opera >= 23',
+    'ios >= 7',
+    'android >= 4.4',
+    'bb >= 10'
+  ];
+
+  return gulp.src(sassFiles)
+    .pipe(sass().on('error', sass.logError))
+    .pipe(autoprefixer({browsers: AUTOPREFIXER_BROWSERS}))
+    .pipe(cssNano())
+    .pipe(gulp.dest('dist/css'));
 });
